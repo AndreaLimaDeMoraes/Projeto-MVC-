27433d7de85466858e4895ad516326d1a2029e4d
lucasBarros-hyke <lucas_barros2022@outlook.com>
2025-10-12 12:31:53 -0300

    Correção no atributo semestre em disciplina para ser INT

--- src/br/edu/fatecgru/mvcaluno/model/Disciplina.java
+++ src/br/edu/fatecgru/mvcaluno/model/Disciplina.java
@@ -1,59 +1,59 @@
 package br.edu.fatecgru.mvcaluno.model;
 
 public class Disciplina {
-	private int idDisciplina;
-	private int idCurso;
-	private String nome;
-	private String semestre;
-	private boolean ativo;
+    private int idDisciplina;
+    private int idCurso;
+    private String nome;
+    private int semestre; // ALTERADO PARA INT
+    private boolean ativo;
 
-	public Disciplina() {}
+    public Disciplina() {}
 
-	public Disciplina(int idDisciplina, int idCurso, String nome, String semestre, boolean ativo) {
-		this.idDisciplina = idDisciplina;
-		this.idCurso = idCurso;
-		this.nome = nome;
-		this.semestre = semestre;
-		this.ativo = ativo;
-	}
+    public Disciplina(int idDisciplina, int idCurso, String nome, int semestre, boolean ativo) {
+        this.idDisciplina = idDisciplina;
+        this.idCurso = idCurso;
+        this.nome = nome;
+        this.semestre = semestre;
+        this.ativo = ativo;
+    }
 
-	public int getIdDisciplina() {
-		return idDisciplina;
-	}
+    public int getIdDisciplina() {
+        return idDisciplina;
+    }
 
-	public void setIdDisciplina(int idDisciplina) {
-		this.idDisciplina = idDisciplina;
-	}
+    public void setIdDisciplina(int idDisciplina) {
+        this.idDisciplina = idDisciplina;
+    }
 
-	public int getIdCurso() {
-		return idCurso;
-	}
+    public int getIdCurso() {
+        return idCurso;
+    }
 
-	public void setIdCurso(int idCurso) {
-		this.idCurso = idCurso;
-	}
+    public void setIdCurso(int idCurso) {
+        this.idCurso = idCurso;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public String getSemestre() {
-		return semestre;
-	}
+    public int getSemestre() {
+        return semestre;
+    }
 
-	public void setSemestre(String semestre) {
-		this.semestre = semestre;
-	}
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
 
-	public boolean isAtivo() {
-		return ativo;
-	}
+    public boolean isAtivo() {
+        return ativo;
+    }
 
-	public void setAtivo(boolean ativo) {
-		this.ativo = ativo;
-	}
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
 }
